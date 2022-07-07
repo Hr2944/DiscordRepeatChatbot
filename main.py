de12e3933a7cc7e4d4f92c6194d6cc69c3998fff
Hugo Riou--Bourdoncle <riou.hugo@yahoo.com>
2022-07-07 13:40:06 +0200

    Update commit

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import os
+
 from botclient import BotClient
 
 if __name__ == '__main__':
-    with open('bot.key', 'r') as token_file:
-        CLIENT_TOKEN = token_file.read().strip()
+
+    try:
+        with open('bot.key', 'r') as token_file:
+            CLIENT_TOKEN = token_file.read().strip()
+    except FileNotFoundError:
+        CLIENT_TOKEN = os.environ.get('CLIENT_TOKEN', None)
     bot = BotClient()
     bot.run(CLIENT_TOKEN)
